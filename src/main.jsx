f30ac14eeeacbc01196dad781d53e56723a2d743
sohail <qazisohailabbas5512@gmail.com>
2025-06-26 12:45:25 +0500

    chnages

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/embed" element={<EmbedBot />} />
+        
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
